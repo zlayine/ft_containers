938ac381f26f3f005b8395f6dfa91aea157ec909
Zouheir Layine <zlayine@e2r5p5.1337.ma>
2021-01-09 12:31:00 +0100

    working on functions list

--- List.hpp
+++ List.hpp
@@ -8,7 +8,7 @@ class ListIterator
 {
 public:
 	typedef typename List::value_type		value_type;
-	typedef typename List::allocator_type*	pointer_type;
+	typedef typename List::node_type*		pointer_type;
 	typedef typename List::value_type&		reference_type;
 
 	ListIterator(pointer_type ptr) : _ptr(ptr)
@@ -90,8 +90,8 @@ private:
 public:
 
 	typedef T								value_type;
-	typedef Node<T>							allocator_type;
-	typedef Node<T>&						reference;
+	typedef Node<T>							node_type;
+	typedef T&								reference;
 	typedef const Node<T>&					const_reference;
 	typedef Node<T>*						pointer;
 	typedef const Node<T>*					const_pointer;
@@ -106,58 +106,206 @@ public:
 	List(List<T> const &src);
 	~List();
 
-	iterator			begin();
-	iterator			end();
-	reverse_iterator	rbgein();
-	reverse_iterator	rend();
-	
-	bool				empty() const;
-	size_type			size() const;
-	size_type			max_size() const;
-	
-	reference			front();
-	const_reference		front() const;
-	reference			back();
-	const_reference		back() const;
-	
-	// template <class InputIterator>
- 	// void assign (InputIterator first, InputIterator last);
-	void 				assign (size_type n, const value_type& val);
-	void				push_front(const T& val);
-	void				pop_front();
-	void				push_back(const T& val);
-	void				pop_front();
-	iterator 			insert(iterator position, const value_type& val);
-	iterator			erase (iterator position);
-	iterator			erase (iterator first, iterator last);
-	void 				swap(List<T>& x);
-	void 				resize(size_type n, value_type val = value_type());
-	void				clear();
+	iterator			begin()
+	{
+		return iterator(_head);
+	}
 
-	void				splice(iterator position, List<T>& x);
-	void				splice(iterator position, List<T>& x, iterator i);
-	void				splice(iterator position, List<T>& x, iterator first, iterator last);
-	void				remove(const value_type& val);
-// 	template <class Predicate>
-//   void remove_if (Predicate pred);
-	void				unique();
-	// template <class BinaryPredicate>
-  	// void unique (BinaryPredicate binary_pred);
-	void				merge(List<T>& x);
-	// template <class Compare>
-	// void merge (List<T>& x, Compare comp);
-	void				sort();
-	// template <class Compare>
-	// void sort (Compare comp);
-	void				reverse();
+	iterator			end()
+	{
+		return iterator(_tail);
+	}
 
-	List<T>&		operator=(const List<T>& lhs);
-	bool			operator==(const List<T>& rhs);
-	bool			operator>=(const List<T>& rhs);
-	bool			operator>(const List<T>& rhs);
-	bool			operator<=(const List<T>& rhs);
-	bool			operator<(const List<T>& rhs);
-	bool			operator!=(const List<T>& rhs);
+	reverse_iterator	rbgein()
+	{
+		return iterator(_tail);
+	}
+
+	reverse_iterator	rend()
+	{
+		return iterator(_head);
+	}
+	
+	bool				empty() const
+	{
+		return _size == 0;
+	}
+
+	size_type			size() const
+	{
+		return _size;
+	}
+
+	// not done
+	size_type			max_size() const
+	{
+		return _size;
+	}
+	
+	reference			front()
+	{
+		return _head->next;
+	}
+
+	const_reference		front() const
+	{
+		return _head->next;
+	}
+
+	reference			back()
+	{
+		return _tail->prev;
+	}
+
+	const_reference		back() const
+	{
+		return _tail->prev;
+	}
+	
+ 	void				assign(const_iterator first, const_iterator last)
+	{
+
+	}
+
+	void 				assign(size_type n, const value_type& val)
+	{
+
+	}
+
+	//not done
+	void				push_front(const T& val)
+	{
+		Node<T> *n;
+		n = _tail;
+		n->data = val;
+		_tail = new Node<T>();
+		_tail->prev = n;
+		n->next = _tail;
+		_size++;
+	}
+
+	void				pop_front()
+	{
+
+	}
+
+	void				push_back(const T& val)
+	{
+		Node<T> *n;
+		n = _tail;
+		n->data = val;
+		_tail = new Node<T>();
+		_tail->prev = n;
+		n->next = _tail;
+		_size++;
+	}
+
+	void				pop_front()
+	{
+
+	}
+
+	iterator 			insert(iterator position, const value_type& val)
+	{
+
+	}
+
+	iterator			erase (iterator position)
+	{
+
+	}
+
+	iterator			erase (iterator first, iterator last)
+	{
+
+	}
+
+	void 				swap(List<T>& x)
+	{
+
+	}
+
+	void 				resize(size_type n, value_type val = value_type())
+	{
+
+	}
+
+	void				clear()
+	{
+
+	}
+
+
+	void				splice(iterator position, List<T>& x)
+	{
+
+	}
+
+	void				splice(iterator position, List<T>& x, iterator i)
+	{
+
+	}
+
+	void				splice(iterator position, List<T>& x, iterator first, iterator last)
+	{
+
+	}
+
+	void				remove(const value_type& val)
+	{
+
+	}
+
+	template <class Predicate>
+  	void				remove_if (Predicate pred)
+	  {
+
+	  }
+
+	void				unique()
+	{
+
+	}
+
+	template <class BinaryPredicate>
+  	void				unique(BinaryPredicate binary_pred)
+	  {
+
+	  }
+
+	void				merge(List<T>& x)
+	{
+
+	}
+
+	template <class Compare>
+	void				merge(List<T>& x, Compare comp)
+	{
+
+	}
+
+	void				sort()
+	{
+
+	}
+
+	template <class Compare>
+	void				sort(Compare comp)
+	{
+
+	}
+
+	void				reverse()
+	{
+
+	}
+
+
+	List<T>&		operator=(const List<T>& lhs)
+	{
+
+	}
+	
 };
 
 template<typename T>
@@ -190,18 +338,6 @@ void		List<T>::push_back(const T& val)
 	_tail->prev = n;
 	n->next = _tail;
 	_size++;
-}
-
-template<typename T>
-List<T>::iterator	List<T>::begin()
-{
-	return iterator(_head);
-}
-
-template<typename T>
-List<T>::iterator	List<T>::end()
-{
-	return iterator(_tail);
 }
 
 #endif
